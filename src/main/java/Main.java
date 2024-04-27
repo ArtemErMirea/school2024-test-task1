93b36f16757c8af4363043edc7704f2dc51d8b84
ArtemErMirea <artem.e.2001@gmail.com>
2024-04-27 16:43:44 +0300

    Main, пока выводит список заказов

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            File file = new File("format.json");
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<Order> orderList = objectMapper.readValue(file, new TypeReference<List<Order>>() {});
+            for (Order order : orderList) {
+                System.out.println(order);
+            }
+            System.out.println (
+                    Reporter.reportBiggestMonth(orderList)
+            );
+
+
+        } catch (IOException e){
+            System.out.println("No file found");
+            e.printStackTrace();
+        }
+    }
+}
