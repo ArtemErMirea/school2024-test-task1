e7a77a9c6a1b9d30d2f45a86f24b035c544140fb
ArtemErMirea <artem.e.2001@gmail.com>
2024-04-27 20:33:26 +0300

    Исправлена сортировка по месяцам в году

--- src/main/java/MonthReported.java
+++ src/main/java/MonthReported.java
@@ -2,6 +2,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Month;
 import java.util.List;
 
 @AllArgsConstructor
--- src/main/java/Reporter.java
+++ src/main/java/Reporter.java
@@ -2,11 +2,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.math.BigDecimal;
+import java.time.Month;
 import java.time.format.TextStyle;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Reporter {
@@ -14,16 +12,16 @@ public class Reporter {
     // формирует отчёт
     public static String reportBiggestMonth(ObjectMapper objectMapper, List<Order> orderList) {
         // Map, хранящий месяц как ключ и сумму заказов за него как значение
-        Map<String, BigDecimal> monthMap = orderList.stream()
+        Map<Month, BigDecimal> monthMap = orderList.stream()
                 // Выбирает только завершённые заказы
                 .filter(order -> order.getStatus().equals("COMPLETED"))
+                .sorted(Comparator.comparing(Order::getOrderedAt))
                 // Собирает элементы в коллекцию
                 .collect(
                         //Группировка по месяцам
                         Collectors.groupingBy(
                                 // Месяц в текстовом формате, маленькми буквами
-                                order -> order.getOrderedAt().getMonth().getDisplayName(
-                                        TextStyle.FULL, Locale.UK).toLowerCase(),
+                                order -> order.getOrderedAt().getMonth(),
                         // Сложение сумм заказов путём свёртки
                         Collectors.reducing (BigDecimal.ZERO, Order::getTotal, BigDecimal::add)
                         )
@@ -34,12 +32,14 @@ public class Reporter {
                 .max(BigDecimal::compareTo)
                 .orElse(BigDecimal.ZERO);
 
-
         //  Проходит по наборам ключ-значение
         //  Месяц, за который сумма заказов равна максимальной, попадает в список
         List<String> monthsWithMaxTotal = monthMap.entrySet().stream()
                 .filter(entry -> entry.getValue().compareTo(maxTotal) == 0)
                 .map(Map.Entry::getKey)
+                .sorted()
+                .map(entry -> entry.getDisplayName(
+                        TextStyle.FULL, Locale.UK).toLowerCase())
                 .toList();
 
         // Создаём экземпляр класса, содержащего список месяцев
